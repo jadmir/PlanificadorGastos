432c741e9cc6ba56d902175151803b4aed020e69
JADMIR IGNACIO VICENTE SIFUENTES <jvicentes@utp.edu.pe>
2023-07-11 21:53:23 -0500

    segundo commit

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,31 @@
 <script setup>
+  import { ref, reactive } from 'vue'
   import Presupuesto from "./components/Presupuesto.vue";
+  import ControlPresupuesto from './components/ControlPresupuesto.vue';
+  import Modal from './components/Modal.vue'
+  import iconoNuevoGasto from './assets/img/nuevo-gasto.svg'
+
+  const modal = reactive({
+    mostrar: false,
+    animar: false
+  })
+  const presupuesto = ref(0)
+  const disponible = ref(0)
+
+  const definirPresupuesto = cantidad => {
+    presupuesto.value = cantidad
+    disponible.value = cantidad
+  }
+
+  const mostrarModal = () => {
+    modal.mostrar = true
+    modal.animar = true
+  }
+
+  const ocultarModal = () => {
+    modal.mostrar = false
+    modal.animar = false
+  }
 </script>
 
 <template>
@@ -9,12 +35,32 @@
 
       <div class="contenedor-header contenedor sombra">
         <Presupuesto 
-      
-      
+        v-if="presupuesto === 0"
+          @definir-presupuesto="definirPresupuesto"      
+        />
+        <ControlPresupuesto 
+          v-else
+          :presupuesto="presupuesto"
+          :disponible="disponible"
         />
       </div>
       
     </header>
+
+    <main v-if="presupuesto > 0">
+
+      <div class="crear-gasto">
+        <img 
+          :src="iconoNuevoGasto"
+          alt="icono nuevo gasto"
+          @click="mostrarModal"
+        />
+      </div>
+      <Modal 
+        v-if="modal.mostrar"
+        @ocultar-modal="ocultarModal"
+      />
+    </main>
   </div>
 </template>
 
@@ -71,5 +117,15 @@
     background-color: var(--blanco);
     border-radius: 1.2rem;
     padding: 5rem;
+  }
+
+  .crear-gasto{
+    position: fixed;
+    bottom: 5rem;
+    right: 5rem;
+  }
+  .crear-gasto img {
+    width: 5rem;
+    cursor: pointer;
   }
 </style>
--- src/components/Presupuesto.vue
+++ src/components/Presupuesto.vue
@@ -6,14 +6,20 @@
 
     const error = ref('')
 
+    const emit = defineEmits(['definir-presupuesto'])
+
     const definirPresupuesto = () => {
-        if(presupuesto.value <= 0) {
+        if(presupuesto.value <= 0 || presupuesto.value === '') {
             error.value = 'Presupuesto no valido'
 
             setTimeout(() => {
                 error.value = ''
             }, 3000);
+
+            return
         }
+
+        emit('definir-presupuesto', presupuesto.value)
     }
 </script>
 
